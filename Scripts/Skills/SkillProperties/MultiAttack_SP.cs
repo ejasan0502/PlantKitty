67deb7d80ba1a72e7c71f668ba8cf950b1e8402f
Eric <ejasan0502@gmail.com>
2019-06-29 22:16:34 -0700

    added multi attack spell property

---
+++ Scripts/Skills/SkillProperties/MultiAttack_SP.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PlantKitty.Scripts.Data;
+
+namespace PlantKitty.Scripts.Skills.SkillProperties
+{
+    public class MultiAttack : SkillProperty
+    {
+        public bool percent;
+        public DamageType dmgType;
+        public float inflict;
+        public int hitCount;
+
+        public override string Description()
+        {
+            string p = percent ? "%" : "";
+            return $"Deals {inflict}{p} {dmgType.ToString()} damage up to {hitCount} times.";
+        }
+
+        public override void Apply(Character caster, Character target, ref string log)
+        {
+            float rawDmg = dmgType == DamageType.physical ? caster.currentStats.PATK : caster.currentStats.MATK;
+
+            for (int i = 0; i < hitCount; i++)
+            {
+                Random random = new Random();
+                if (random.Next(0, 100) <= caster.currentStats.CRIT)
+                {
+                    rawDmg *= caster.currentStats.CRITDMG;
+                    log += "CRITICAL\n";
+                }
+
+                if (percent)
+                    rawDmg *= inflict;
+                else
+                    rawDmg += inflict;
+
+                float dmg = rawDmg - (dmgType == DamageType.physical ? target.currentStats.PDEF : target.currentStats.MDEF);
+                if (dmg < 1f) dmg = 1f;
+
+                target.Hit(dmg);
+                log += $"{target.name} takes {dmg} {dmgType.ToString()} damage!";
+            }
+        }
+
+        public override string ToDataString()
+        {
+            return $"MultiAttack$percent>{percent}$dmgType>{dmgType.ToString()}$inflict>{inflict}$hitCount>{hitCount}";
+        }
+    }
+}
